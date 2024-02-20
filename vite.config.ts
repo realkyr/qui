8297af761cfa05bf4e8a9be976af80c63058976f
realkyr <phuree.kanusont@gmail.com>
2024-02-20 16:39:41 +0700

    Add initial setup for Vite, React and TypeScript project
    This commit sets up a new project using Vite with React and TypeScript. Additionally, it includes an ESLint config, .gitignore file, and a README. The project includes a simple app with css styling and svg logos.

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  plugins: [react()],
+  build: mode === 'package'
+    ? {
+      lib: {
+        entry: 'src/lib/index.ts',
+        name: 'qui',
+        formats: ['es', 'umd'],
+      },
+      rollupOptions: {
+        external: ['react', 'react-dom'],
+        output: {
+          globals: {
+            react: 'React',
+            'react-dom': 'ReactDOM',
+          },
+        },
+      },
+    } : {},
+}))
